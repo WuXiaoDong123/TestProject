60fb8ce337f04e161576d0898edb4fe1e43edbad
wujialin <wujialin2362@dingtalk.com>
2020-05-10 15:18:34 +0800

    添加第一个控制器

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Administrator
+ */
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @GetMapping("/userInfo")
+    public Map<String, Object> flushDb() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "小强");
+        map.put("age", 18);
+        return map;
+    }
+}
